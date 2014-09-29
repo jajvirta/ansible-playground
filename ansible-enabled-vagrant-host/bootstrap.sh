815e942e1b31d0644bc953e1741e04953075b6f2
Jarno Virtanen <jarno.virtanen@solita.fi>
2014-09-29 22:34:17 +0300

    bootstrap ansible on host

--- ansible-enabled-vagrant-host/bootstrap.sh
+++ ansible-enabled-vagrant-host/bootstrap.sh
@@ -4,4 +4,9 @@ apt-get update
 apt-get install -y python-dev python-pip 
 pip install ansible
 
+mkdir /etc/ansible
+echo "[local]" > /etc/ansible/hosts
+echo "127.0.0.1" >> /etc/ansible/hosts
+
+ansible-playbook /srv/playbooks/site.yml
 
