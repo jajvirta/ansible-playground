bab7fddedbc996ffda85433ad447b0644849fbab
Jarno Virtanen <jarno.virtanen@solita.fi>
2014-10-02 00:03:13 +0300

    nicer syntax for adding initial hosts and host_vars

--- ansible-enabled-vagrant-host/bootstrap.sh
+++ ansible-enabled-vagrant-host/bootstrap.sh
@@ -3,8 +3,21 @@
 yum install -y ansible
 yum update -y gmp
 
-echo "[local]" > /etc/ansible/hosts
-echo "127.0.0.1" >> /etc/ansible/hosts
+cat <<EOF > /etc/ansible/hosts
+[local]
+127.0.0.1
+EOF
+
+mkdir /etc/ansible/host_vars
+cat <<EOF > /etc/ansible/host_vars/127.0.0.1
+eee_db_url: jdbc@foobar
+eee_db_user: eee
+eee_db_password: f%3djlk34
+
+aaa_db_url: jdbc@foobar
+aaa_db_user: aaa
+aaa_db_password: f%3djlk34
+EOF
 
 ansible-playbook /srv/playbooks/site.yml
 
