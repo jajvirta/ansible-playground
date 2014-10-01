f6ae703768cc68ba981f33a269e397662854f19d
Jarno Virtanen <jarno.virtanen@solita.fi>
2014-10-01 21:44:22 +0300

    switch to centos 6.5 box

--- ansible-enabled-vagrant-host/bootstrap.sh
+++ ansible-enabled-vagrant-host/bootstrap.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y python-dev python-pip 
-pip install ansible
+yum install -y ansible
+yum update -y gmp
 
-mkdir /etc/ansible
 echo "[local]" > /etc/ansible/hosts
 echo "127.0.0.1" >> /etc/ansible/hosts
 
